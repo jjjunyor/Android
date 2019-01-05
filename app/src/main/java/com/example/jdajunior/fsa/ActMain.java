07c26de8ff805dd20088813caefa93c54b53c6d0
jjjunyor <jjjunyor@gmail.com>
2019-01-05 09:02:32 -0200

    teste de commit

--- app/src/main/java/com/example/jdajunior/fsa/ActMain.java
+++ app/src/main/java/com/example/jdajunior/fsa/ActMain.java
@@ -20,8 +20,9 @@ public class ActMain extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Replace with your own actions", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+//teste
             }
         });
     }
